351c34a6a4863d22f2df8c6a0241ceebbc2537c4
matteo_turla <mturla13@gmail.com>
2022-06-26 18:39:19 +0200

    added notebook to run project

--- download_data.py
+++ download_data.py
@@ -1,9 +1,10 @@
 import os
+os.environ['KAGGLE_USERNAME'] = "matteoturla"
+os.environ['KAGGLE_KEY'] = "f9ee66d5fb9648caf98f980b3108f94d"
+    
 import kaggle 
 
 if __name__ == '__main__':
 
-    os.environ['KAGGLE_USERNAME'] = "matteoturla"
-    os.environ['KAGGLE_KEY'] = "f9ee66d5fb9648caf98f980b3108f94d"
     kaggle.api.authenticate()
     kaggle.api.dataset_download_files('defileroff/comic-faces-paired-synthetic-v2', path='data', unzip=True)
